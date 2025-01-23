23a33d280c7c259bfda3d69a89814c5b5bd434e3
Everton Silva <evertom.silva@outlook.com.br>
2025-01-23 13:11:14 -0300

    save

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 
-print ("""
+def exibir_nome_programa():
+    print ("""
 ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
 ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
 ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
@@ -8,25 +9,31 @@ print ("""
 ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
 ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░\n""")
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. Ativar restaurante')
-print('4. Sair\n')
-
-opcao_escolhida = int(input('Digite a opção desejada: '))
+def menu():
+    print('1. Cadastrar restaurante')
+    print('2. Listar restaurante')
+    print('3. Ativar restaurante')
+    print('4. Sair\n')
 
 def finalizar_app():
     os.system('cls')
-    print('\nFinalizando o app...')
-    
+    print('\nFinalizando o app...\n')
 
+def escolher_opcao(): 
+    opcao_escolhida = int(input('Digite a opção desejada: '))
+    if opcao_escolhida == 1:
+        print('\nCadastrando novo restaurante...')
+    elif opcao_escolhida == 2:
+        print('\nListando restaurantes...')
+    elif opcao_escolhida == 3:
+        print('\nAtivando restaurante...')
+    else:
+        finalizar_app()
 
+def main():
+    exibir_nome_programa()
+    menu()
+    escolher_opcao()
 
-if opcao_escolhida == 1:
-    print('\nCadastrando novo restaurante...')
-elif opcao_escolhida == 2:
-    print('\nListando restaurantes...')
-elif opcao_escolhida == 3:
-    print('\nAtivando restaurante...')
-else:
-    finalizar_app()
+if __name__ == '__main__':
+    main()
